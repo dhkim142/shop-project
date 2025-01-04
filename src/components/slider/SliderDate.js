eb357309b2da6d1635395e1cdf514659858dc4fe
Dohyun Kim <rlaehgus142@gmail.com>
2025-01-03 23:57:37 -0800

    1/3/2025

---
+++ src/components/slider/SliderDate.js
@@ -0,0 +1,21 @@
+const sliderData = [
+    {
+        image: 'https://plus.unsplash.com/premium_photo-1727881363980-eaf6432bafa3?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        heading: 'title 1',
+        desc: 'description 1'
+    },
+{
+        image: "https://images.unsplash.com/photo-1735779411146-a2a6ce587cbb?q=80&w=1960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        heading: 'title 2',
+        desc: 'description 2'
+    },
+
+    
+    {
+        image: 'https://plus.unsplash.com/premium_photo-1683288446489-71997f24255d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        heading: 'title 3',
+        desc: 'description 3'
+    },
+]
+
+export default sliderData;
